1d70a2776080f94ccf6a7667711ff5a19a36fede
Lesley Burdock <burdocklesley@gmail.com>
2019-12-19 17:25:28 +0100

    trying to deploy

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+root to: 'cocktails#index'
 
-  resources :cocktails do
+  resources :cocktails, :except => :index do
     resources :doses, only: [:index, :new, :create, :destroy]
     resources :reviews, only: [:index, :new, :create]
   end
